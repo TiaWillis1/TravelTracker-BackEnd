a39eb05ade69c4ad0b73455a1d8b3ad02b343de3
Sunny <1sunnymuniz@gmail.com>
2023-01-23 12:01:10 -0700

    created a folder for app, created a folder for models and files for a profile and pin model

---
+++ app/__init__.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI")
+
+    # Import models here for Alembic setup
+    from app.models.pin import Pin
+    from app.models.profile import Profile
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import boards_bp
+    app.register_blueprint(boards_bp)
+
+    # Register Blueprints here
+    # from .routes import example_bp
+    # app.register_blueprint(example_bp)
+
+    CORS(app)
+    return app
+
