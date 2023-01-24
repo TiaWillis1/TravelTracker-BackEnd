16013d5fa75930dd22c2ebe780c1d3a843091246
Sunny <1sunnymuniz@gmail.com>
2023-01-24 11:21:11 -0700

    modified model profile and changed the name for pin attribute

--- app/models/profile.py
+++ app/models/profile.py
@@ -4,5 +4,5 @@ from app import db
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String)
-    cards = db.relationship("Pin", back_populates="profile")
+    pins = db.relationship("Pin", back_populates="profile")
     name = db.Column(db.String) 
